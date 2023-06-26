18955e8d0287c625dbb989d6511a25e0fab1eb2a
Even Koshliak <evenkoshliak@o-s-i.org>
2023-06-26 17:13:21 +0300

    TS - rendering project lists

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import ProjectInput from './project-input';
+import ProjectList from './project-list';
 
 import './style.css'
 
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
 
